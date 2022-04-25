c365eee65d75840001f0686d68f61f6eace8dc9d
test@test.com <test@test.com>
2022-04-25 11:33:19 +0300

    test changes

---
+++ src/components/ui/UICard.vue
@@ -0,0 +1,37 @@
+<template>
+    <form class="form-horizontal">
+        <span class="heading">АВТОРИЗАЦИЯ</span>
+    </form>
+</template>
+
+<style scoped lang="scss">
+@import "src/styles/colors";
+
+.form-horizontal{
+    background: $white;
+    padding-bottom: 30px;
+    border-radius: 15px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    box-shadow: 1px 1px 6px $gray;
+   }
+
+.form-horizontal .heading{
+    font-size: 35px;
+    font-weight: 700;
+    padding: 35px 0;
+    border-bottom: 1px solid $gray-100;
+    margin-bottom: 30px;
+    text-align: center;
+   }
+
+@media only screen and (max-width: 1023px){
+.form-horizontal {
+            border-radius: 8px;
+            box-shadow: none;
+            border: 1px solid $gray-200;
+}
+
+}
+</style>
--- src/components/ui/register.js
+++ src/components/ui/register.js
@@ -1,5 +1,7 @@
 import UIButton from "@/components/ui/UIButton";
+import UICard from "@/components/ui/UICard";
 
 export default [
-    UIButton
+    UIButton,
+    UICard
 ]
